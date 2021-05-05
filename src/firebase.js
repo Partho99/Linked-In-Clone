81f705b2ccd1e63fbe16e05dfe171b8b12f360e5
Partho Das <partho.swe@gmail.com>
2021-05-05 17:21:36 +0600

    firebase configuration added and using firestore database also

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCQE_VelU3RYpnD6i5yI7X3wD_oT84cEVw",
+    authDomain: "linked-in-demo.firebaseapp.com",
+    projectId: "linked-in-demo",
+    storageBucket: "linked-in-demo.appspot.com",
+    messagingSenderId: "1098871626202",
+    appId: "1:1098871626202:web:687af0f6d41986722008e3"
+};

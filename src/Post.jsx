a859cd7484804e103e8d82e77c684e64eaaf6eb8
Partho Das <partho.swe@gmail.com>
2021-05-05 12:40:43 +0600

    design of post and like share send comment design completed

---
+++ src/Post.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Avatar} from "@material-ui/core";
+import './Post.css';
+import InputOption from "./InputOption";
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined'
+import ChatOutlinedIcon from "@material-ui/icons/ChatOutlined";
+import ShareOutlinedIcon from "@material-ui/icons/ShareOutlined";
+import SendOutlinedIcon from "@material-ui/icons/SendOutlined";
+
+const Post = ({name, description, message, photoUrl}) => {
+    return (
+        <div className='post'>
+            <div className='post__header'>
+                <Avatar/>
+                <div className='post__info'>
+                    <h2>{name}</h2>
+                    <p>{description}</p>
+                </div>
+            </div>
+            <div className='post__body'>
+                <p>{message}</p>
+            </div>
+            <div className='post__buttons'>
+                <InputOption
+                    Icon={ThumbUpAltOutlinedIcon}
+                    title='Like'
+                    /*color='blue'*/
+                />
+                <InputOption
+                    Icon={ChatOutlinedIcon}
+                    title='Comment'
+                />
+                <InputOption
+                    Icon={ShareOutlinedIcon}
+                    title='Share'/>
+                <InputOption
+                    Icon={SendOutlinedIcon}
+                    title='Send'
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Post;

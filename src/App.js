073ed2641165c345b6acf19303fcd799556784a0
Partho Das <partho.swe@gmail.com>
2021-05-06 14:19:24 +0600

    redux store user selector added for global user state management

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
+import {useDispatch, useSelector} from "react-redux";
+import {login, selectUser} from "./features/userSlice";
+import Login from "./Login";
+import {auth} from "./firebase";
 
 function App() {
+    const user = useSelector(selectUser);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        auth.onAuthStateChanged((userAuth) => {
+            if (userAuth) {
+                dispatch(login({
+                    email: userAuth.email,
+                    uid: userAuth.uid,
+                    displayName: userAuth.displayName,
+                    photoUrl: userAuth.photoURL
+                }))
+            }
+        })
+    }, [])
     return (
         <div className="app">
             <Header/>
-            <div className='app__body'>
-                <Sidebar />
-                <Feed />
-            </div>
-            {/* Feed */}
-            {/* Widgets */}
+            {!user ? (
+                <Login/>
+            ) : (
+                <div className='app__body'>
+                    <Sidebar/>
+                    <Feed/>
+                </div>
+            )}
         </div>
     );
 }
